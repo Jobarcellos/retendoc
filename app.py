4793197cb4cf115bd3df85174a6d97278f898cad
Jobarcellos <joelmabarcellos@gmail.com>
2026-06-18 23:00:44 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,3 @@
-st.set_page_config(
-    page_title="RegDoc — Regularidade Docente",
-    page_icon="📊",
-    layout="wide",
-    initial_sidebar_state="expanded"
-)
-
 import streamlit as st
 
 st.set_page_config(
